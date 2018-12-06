f210e3052272611013177ec3de9b6f5037dc194e
Anh Trinh <anh.trinhtrung@gmail.com>
2018-12-07 00:43:07 +1300

    First commit

---
+++ PRESENTATION.md
@@ -0,0 +1,34 @@
+KEYS TAKEAWAYS:
+
+### Trying to use id as your keys!
+
+
+### If your list is static, then it's fine using index
+
+
+### prefer PureComponent over Component, but never mutate your objects (and use other best practices)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class TodoItem extends Component {
+  constructor(props) {
+    super(props)
+    this.renderCount = 0
+    this.textInput = React.createRef();
+    this.focusTextInput = this.focusTextInput.bind(this);
+  }
+
+  componentDidMount() {
+    this.focusTextInput()
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.todo !== this.props.todo
+  }
+
+  focusTextInput() {
+    // Explicitly focus the text input using the raw DOM API
+    // Note: we're accessing "current" to get the DOM node
+    this.textInput.current.focus();
+  }
+
   render() {
+    const { todo, index, onChange } = this.props
+    this.renderCount ++
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Todo-item">
+        {index && <span className="Todo-number">{index + 1}.</span>}
+        <div className="Input">
+          <input
+            type="text"
+            className="Input-text"
+            ref={this.textInput}
+            value={todo.name}
+            onChange={(e) => onChange(todo, e.target.value)}
+          />
+        </div>
+        <span className="Todo-render">render {this.renderCount} times</span>
+      </div>
+    )
+  }
+}
+
+let globalId = 1
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      todos: [{ id: globalId, name: 'First task!' }]
+    }
+
+    this.onAdd = this.onAdd.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  onAdd() {
+    globalId ++;
+
+    this.setState({
+      todos: [
+        {id: globalId, name: '', done: false},
+        ...this.state.todos,
+      ]
+    })
+  }
+
+  handleChange(todo, name) {
+    const todos = this.state.todos.map(td => {
+      if (td === todo) {
+        return {
+          ...td,
+          name
+        }
+      }
+      return td;
+    })
+
+    this.setState({ todos })
+  }
+
+  render() {
+    const { todos } = this.state
+
+    return (
+      <div className="Wrapper">
+        <button onClick={this.onAdd}>Add Todo</button>
+        <div className="Todo-wrapper">
+          <h1 className="Title">Key = index</h1>
+          {
+            todos.map((todo, idx) => (
+              <TodoItem key={todo.id} todo={todo} onChange={this.handleChange}/>
+            ))
+          }
+        </div>
       </div>
     );
   }
